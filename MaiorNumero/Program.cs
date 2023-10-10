fad643ae9962f850906dae18702a3706038eafa9
Isaque Barbosa da Silva <isaque_barbosa@jabil.com>
2023-10-10 07:39:22 -0300

    primeiro commit

---
+++ MaiorNumero/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+string[] valores1 = Console.ReadLine().Split(' ');
+double x1 = double.Parse(valores1[0]);
+double y1 = double.Parse(valores1[1]);
+
+string[] valores2 = Console.ReadLine().Split(' ');
+double x2  = double.Parse(valores2[0]);
+double y2 = double.Parse(valores2[1]);
+
+double x = x1 - x2;
+double y = y1 - y2;
+
+double distancia = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+Console.WriteLine(distancia.ToString("F4", CultureInfo.InvariantCulture));
